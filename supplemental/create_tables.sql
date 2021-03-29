1e38386a4439376268db7cb0851b677886ba656b
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2021-03-29 17:43:36 -0500

    Update to reflect new diagram
    Changed tables to reflect the new diagram. I also formatted the SQL to make it a little easier to read,

--- supplemental/create_tables.sql
+++ supplemental/create_tables.sql
@@ -4,71 +4,62 @@ CREATE TABLE extra_charge (
   weight   DOUBLE (5, 2) NOT NULL,
   shipping DOUBLE (5, 2) NOT NULL,
   handling DOUBLE (5, 2) NOT NULL,
-  PRIMARY KEY (weight));
+  PRIMARY KEY (weight)
+);
   
 CREATE TABLE inventory (
-  number INT NOT NULL PRIMARY KEY,
-  quantity INT NOT NULL);
+  number   INT NOT NULL PRIMARY KEY,
+  quantity INT NOT NULL,
+  FOREIGN KEY(number) REFERENCES parts(number)
+);
   
 CREATE TABLE part_collection (
   part_collection_id INT PRIMARY KEY AUTO_INCREMENT,
-  number INT NOT NULL,
-  quantity INT NOT NULL,
-  FOREIGN KEY (number) REFERENCES inventory(number));
+  order_id           INT NOT NULL,
+  number             INT NOT NULL,
+  quantity           INT NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES order(order_id),
+  FOREIGN KEY (number)   REFERENCES inventory(number)
+);
 
 CREATE TABLE customer(
-	customer_id		int NOT NULL AUTO_INCREMENT,
-	order_collection_id 	int NOT NULL,
-	email			char(255) NOT NULL,
-	first_name		char(255) NOT NULL,
-	last_name 		char(255) NOT NULL,
-	address			char(255) NOT NULL,
-	PRIMARY KEY(customer_id),
-	FOREIGN KEY(order_collection_id) REFERENCES order_collection(order_collection_id)
+	customer_id INT NOT NULL AUTO_INCREMENT,
+	email       CHAR(255) NOT NULL,
+	first_name  CHAR(255) NOT NULL,
+	last_name   CHAR(255) NOT NULL,
+	address     CHAR(255) NOT NULL,
+	PRIMARY KEY(customer_id)
 );
 
 CREATE TABLE order (
    order_id                   INT PRIMARY KEY AUTO_INCREMENT,
    customer_id                INT NOT NULL,
-   part_collection_id         INT NOT NULL, 
    weight                     DOUBLE(5, 2) NOT NULL,
    shipping                   DOUBLE(5, 2) NOT NULL, 
    handling                   DOUBLE(5, 2) NOT NULL, 
    charge_total               DOUBLE(5, 2) NOT NULL, 
    order_date                 TIMESTAMP NOT NULL,
    status                     CHAR(255) NOT NULL,
-   FOREIGN KEY(customer_id)          REFERENCES customer(customer_id),
-   FOREIGN KEY(part_collection_id)   REFERENCES part_collection(part_collection_id)
-);
-
-CREATE TABLE order_collection(
-	order_collection_id 	int NOT NULL AUTO_INCREMENT,
-	customer_id	    	int NOT NULL,
-	order_id 	    	int NOT NULL,
-	PRIMARY KEY(order_collection_id),
-	FOREIGN KEY(customer_id) 	REFERENCES customer(customer_id),
-	FOREIGN KEY(order_id) 		REFERENCES order(order_id)
+   FOREIGN KEY(customer_id) REFERENCES customer(customer_id)
 );
 
 CREATE TABLE account(
-    account_id int NOT NULL AUTO_INCREMENT,
-    position_collection_id NOT NULL,
-    first_name char(255) NOT NULL,
-    last_name char(255) NOT NULL,
-    PRIMARY KEY(account_id, position_collection_id),
-    FOREIGN KEY (position_collection_id) REFERENCES position_collection(position_collection_id)
+    account_id INT NOT NULL AUTO_INCREMENT,
+    first_name CHAR(255) NOT NULL,
+    last_name  CHAR(255) NOT NULL,
+    password   CHAR(255) NOT NULL,
+    PRIMARY KEY(account_id)
 );
 
 CREATE TABLE position_collection(
-    position_collection_id int PRIMARY KEY AUTO_INCREMENT,
-    position_id int NOT NULL,
-    account_id int NOT NULL,
-    account_password char(255) NOT NULL,
+    position_collection_id INT PRIMARY KEY AUTO_INCREMENT,
+    position_id            INT NOT NULL,
+    account_id             INT NOT NULL,
     FOREIGN KEY(position_id) REFERENCES position(position_id),
-    FOREIGN KEY(account_id) REFERENCES account(account_id)
+    FOREIGN KEY(account_id)  REFERENCES account(account_id)
 );
 
 CREATE TABLE position(
-    position_id int PRIMARY KEY AUTO_INCREMENT,
-    position_description char(255) NOT NULL
+    position_id          INT PRIMARY KEY AUTO_INCREMENT,
+    position_description CHAR(255) NOT NULL
 );
