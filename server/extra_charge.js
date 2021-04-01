bc543f8d48447bc03ee2052f94e9ba12d9094d17
DylanFogle <70647762+DylanFogle@users.noreply.github.com>
2021-03-31 19:27:34 -0500

    Add files via upload

---
+++ server/extra_charge.js
@@ -0,0 +1,99 @@
+const express 	= require('express')
+const cors 	= require('cors');
+const router 	= express.Router();
+const db 	= require('./db');
+
+// We need these for the request, response
+router.use(express.json());
+router.use(cors());
+
+router.get('extra_charge/all', (req, res) => {
+    db.query('SELECT * FROM extra_charge', (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+});
+
+router.get('extra_charge/select/:weight', (req, res) => {
+    db.query('SELECT * FROM extra_charge WHERE weight = ?',
+    [weight],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+});
+
+router.post("extra_charge/create", (req, res) => {
+  const weight	 = req.body.weight;
+  const shipping = req.body.shipping;
+  const handling = req.body.handling;
+
+  db.query(
+    "INSERT INTO extra_charge (weight, shipping, handling) VALUES (?,?,?)",
+    [weight, shipping, handling],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("extra_charge table - values inserted");
+      }
+    }
+  );
+});
+
+router.put("extra_charge/update", (req, res) => {
+  const weight	 = req.body.weight;
+  const shipping = req.body.shipping;
+  const handling = req.body.handling;
+
+  if(shipping != null){
+    db.query(
+      "UPDATE extra_charge SET shipping = ? WHERE weight = ?",
+      [shipping, handling, weight],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  }
+  if(handling != null){
+    db.query(
+      "UPDATE extra_charge SET handling = ? WHERE weight = ?",
+      [handling, weight],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  }
+});
+
+router.delete("extra_charge/delete/:weight", (req, res) => {
+  const weight = req.params.weight;
+
+  db.query(
+    "DELETE FROM extra_charge WHERE weight = ?",
+    weight,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+module.exports = router;
---
+++ server/part_collection.js
@@ -0,0 +1,115 @@
+const express 	= require('express')
+const cors 	= require('cors');
+const router 	= express.Router();
+const db 	= require('./db');
+
+// We need these for the request, response
+router.use(express.json());
+router.use(cors());
+
+router.get('part_collection/all', (req, res) => {
+    db.query('SELECT * FROM part_collection', (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+});
+
+router.get('part_collection/select/:part_collection_id', (req, res) => {
+    db.query('SELECT * FROM part_collection WHERE part_collection_id = ?',
+    [part_collection_id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+});
+
+router.post("part_collection/create", (req, res) => {
+  const id       = req.body.part_collection_id;
+  const order_id = req.body.order_id;
+  const number   = req.body.number;
+  const quantity = req.body.quantity;
+
+  db.query(
+    "INSERT INTO part_collection (part_collection_id, order_id, number, quantity)
+    VALUES (?,?,?,?)",
+    [id, order_id, number, quantity],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("part_collection table - values inserted");
+      }
+    }
+  );
+});
+
+router.put("part_collection/update", (req, res) => {
+  const id       = req.body.part_collection_id;
+  const order_id = req.body.order_id;
+  const number   = req.body.number;
+  const quantity = req.body.quantity;
+
+  if(order_id != null){
+    db.query(
+      "UPDATE part_collection SET order_id = ? WHERE part_collection_id = ?",
+      [order_id, id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  }
+  if(number != null){
+    db.query(
+      "UPDATE part_collection SET number = ? WHERE part_collection_id = ?",
+      [number, id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  }
+  if(quantity != null){
+    db.query(
+      "UPDATE part_collection SET quantity = ? WHERE part_collection_id = ?",
+      [quantity, id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  }
+});
+
+router.delete("part_collection/delete/:weight", (req, res) => {
+  const id = req.params.part_collection_id;
+
+  db.query(
+    "DELETE FROM part_collection WHERE part_collection_id = ?",
+    id,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+module.exports = router;
