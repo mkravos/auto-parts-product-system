f1887b7b0be329cbdb2d33c6bd13ea4c03515aad
dL31 <daniellicari0461@gmail.com>
2021-04-15 22:44:13 -0500

    receiving material-tables

--- client/src/components/Receiving/styles.js
+++ client/src/components/Receiving/styles.js
@@ -1,12 +1,22 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '25ch',
-    },
-  },
+   div: {
+      position: "relative",
+      top: "100px"
+   },
+   form: {
+      padding: "30px"
+   },
+   textField: {
+      '& > *': {
+         margin: theme.spacing(1),
+         width: '25ch',
+      },
+   },
+   grid: {
+      padding: "30px"
+   }
 }));
 
 export default useStyles
